6141954a0a4a0380920131ecd1627fb002c60216
mrhub3117 <pathan77000@gmail.com>
2025-05-15 08:51:25 +0530

    Create Singleton.java

---
+++ Singleton.java
@@ -0,0 +1,21 @@
+8)design pattern
+class Singleton {
+    private static Singleton instance;
+
+    private Singleton() {} 
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Singleton obj1 = Singleton.getInstance();
+        Singleton obj2 = Singleton.getInstance();
+        System.out.println(obj1 == obj2); 
+    }
+}
